1d72b1764b1a77f1c6e438d62ea1d74e47f2a4a2
fred <quote@veri.hk>
2019-05-12 16:55:24 +0800

    add 2 funs

--- strout.go
+++ strout.go
@@ -10,3 +10,14 @@ func St1(tr string) string {
 	}
 	return tr
 }
+
+func St2(tr string) string {
+	tr = strings.Replace(tr, ` `, ``, -1)
+	tr = strings.Replace(tr, `,`, `;`, -1)
+	return tr
+}
+
+func St3(tr string) string {
+	tr = strings.Replace(tr, ` `, ``, -1)
+	return tr
+}
